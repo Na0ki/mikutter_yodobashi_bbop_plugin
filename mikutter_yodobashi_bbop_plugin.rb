1804ae99a44b1c2289ef2251c6e4715ac26f3325
ahiru <wellingtonnmaeda0524@gmail.com>
2017-05-17 14:29:02 +0900

    リファクタリング
    rubocop 追加

--- mikutter_yodobashi_bbop_plugin.rb
+++ mikutter_yodobashi_bbop_plugin.rb
@@ -1,30 +1,25 @@
-﻿#-*- coding: utf-8 -*-
+﻿# -*- coding: utf-8 -*-
+# -*- frozen_string_literal: true -*-
 
 Plugin.create :yodobashibbop do
-
-  @elements = ['ヨォ', 'ドォ', 'バァ', 'シィ', 'カァ', 'メェ', 'ラ']
+  @elements = %w[ヨォ ドォ バァ シィ カァ メェ ラ]
 
   # ydop実行(def ydop yodobashibbop)
-  def yodobashibbop
-    result = @elements.size.times.map { @elements.sample }
-    score = getscore(result)
+  def yodobashi_bbop
+    # 並べ替え
+    result = Array.new(@elements.size) { @elements.sample }
+    # スコア計算
+    score  = result.zip(@elements).select { |a, b| a == b }.size / @elements.size.to_f
 
-    "#{result.join} (#{sprintf('%.1f', score*100)}\%)"
-  end
-
-  # スコア計算
-  def getscore(str)
-    str.zip(@elements).select { |a, b| a == b }.size / @elements.size.to_f
+    "#{result.join} (%.1f%%)" % (score * 100)
   end
 
   on_appear do |ms|
     ms.each do |m|
-      if Time.now - m[:created] > 5
-        next
-      end
-      if m.message.to_s =~ /^@#{Service.primary.user.to_s} (yodobashi|ヨドバシ)/
-        # Plugin.activity :system, "#{yodobashibbop}"
-        m.message.post(:message => "@#{m.message.user[:idname]} #{yodobashibbop}")
+      next if Time.now - m[:created] > 5
+
+      if m.to_s =~ /^@#{Service.primary.user.to_s} (yodobashi|ヨドバシ)/
+        m.post(message: "@#{m.user[:idname]} #{yodobashi_bbop}")
       end
     end
   end
